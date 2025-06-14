7a013c03477d0cac3db0d375b00479b39d5c516c
João Victor <elietejvc@hotmail.com>
2025-06-14 02:24:59 -0300

    loops

---
+++ cpp/loops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Digite um número inteiro não negativo\n";
+    cin >> n;
+
+
+    int soma = 0;
+    int cont = 1;
+    while(cont <= soma){
+        soma +=cont;
+        
+        break
+    ;}
+
+    for(int j = 1; j<=n; j+=1){
+        soma += j;
+    }
+
+    do
+    {
+        soma +=cont;
+        cont++;
+    } while (cont <= n);
+    
+} 
